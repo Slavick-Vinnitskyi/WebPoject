592a8ff70a648aa88bea4efbafffb3878dde2e2c
Славик Винницкий <andrey.belyi1@gmail.com>
2018-12-23 14:35:45 +0200

    context initialization moved to ContextListener

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
@@ -20,12 +19,12 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Servlet extends HttpServlet {
 
     private static ServletContext context;
-    private static Map<String, Command> commands = new ConcurrentHashMap<>();
+    private static Map<String, Command> commands;
 
     @Override
     public void init(ServletConfig servletConfig) {
         context = servletConfig.getServletContext();
-        context.setAttribute("loggedUsers", new ConcurrentHashMap<Integer, HttpSession>());
+        commands = new ConcurrentHashMap<>();
 
         commands.put("index", new IndexCommand());
         commands.put("login", new LoginCommand());
